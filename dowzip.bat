1017cc11d0a6812c1281404f6160cf0902093550
min <min@5f0a215f-9dc6-43d0-aa08-c0b9dc37ed8b>
2003-09-24 19:50:39 +0000

    Script files are now in a separate directory (scripts).
    git-svn-id: file:///Users/min/Documents/svnrepros/svn_mindia/trunk@16 5f0a215f-9dc6-43d0-aa08-c0b9dc37ed8b

--- dowzip.bat
+++ dowzip.bat
@@ -1,2 +1,2 @@
-zip -u %1 *.h *.cpp *.c *.ui *.cw *.txt *.html *.py *.xpm *.png *.i *.po *.pro *.qm *.dsp *.dsw *.bat *.ini mindiaall.mak wmindia.mak lmindia.mak _*.mak AUTHORS COPYING HISTORY INSTALL BUGS README setqt2 examples/*.dia -x moc_*
+zip -u %1 *.h *.cpp *.c *.ui *.cw *.txt *.html scripts\*.py *.xpm *.png *.i *.po *.pro *.qm *.dsp *.dsw *.bat *.ini mindiaall.mak wmindia.mak lmindia.mak _*.mak AUTHORS COPYING HISTORY INSTALL BUGS README setqt2 examples/*.dia -x moc_*
 zip -u examples.zip *.dia *.bmp *.jpg
