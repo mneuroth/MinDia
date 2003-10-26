b0b92d0da11043d373e9347042ea9b59b8ab27bf
min <min@5f0a215f-9dc6-43d0-aa08-c0b9dc37ed8b>
2003-10-26 22:38:05 +0000

    setqt3 added
    git-svn-id: file:///Users/min/Documents/svnrepros/svn_mindia/trunk@48 5f0a215f-9dc6-43d0-aa08-c0b9dc37ed8b

--- dozip.bat
+++ dozip.bat
@@ -1,2 +1,2 @@
-zip -u %1 *.h *.cpp *.c *.ui *.cw *.txt *.html scripts\*.py *.xpm *.png *.i *.po *.pro *.qm *.dsp *.dsw *.bat *.ini *.in *.m4 *.control *.desktop installapp configure Makefile dotgz mindiaall.mak wmindia.mak lmindia.mak _*.mak AUTHORS COPYING HISTORY INSTALL BUGS README setqt2 examples/*.dia -x moc_*
+zip -u %1 *.h *.cpp *.c *.ui *.cw *.txt *.html scripts\*.py *.xpm *.png *.i *.po *.pro *.qm *.dsp *.dsw *.bat *.ini *.in *.m4 *.control *.desktop installapp configure Makefile dotgz mindiaall.mak wmindia.mak lmindia.mak _*.mak AUTHORS COPYING HISTORY INSTALL BUGS README setqt2 setqt3 examples/*.dia -x moc_*
 zip -u examples.zip *.dia *.bmp *.jpg
