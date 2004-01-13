db9143c2860810b07470e79d63d3c65a161d1c55
min <min@5f0a215f-9dc6-43d0-aa08-c0b9dc37ed8b>
2004-01-13 19:50:25 +0000

    files for application configuration handling added
    git-svn-id: file:///Users/min/Documents/svnrepros/svn_mindia/trunk@62 5f0a215f-9dc6-43d0-aa08-c0b9dc37ed8b

---
+++ src/appconfig.h
@@ -0,0 +1,41 @@
+/**************************************************************************
+ *
+ *	project				 : MinDia
+ *
+ *	copyright            : (C) 2002 by Michael Neuroth
+ *
+ * ------------------------------------------------------------------------
+ *
+ *  $Source: /Users/min/Documents/home/cvsroot/mindia/src/appconfig.h,v $
+ *
+ *  $Revision: 1.1 $
+ *
+ *	$Log: not supported by cvs2svn $
+ *	
+ *
+ ***************************************************************************/
+/***************************************************************************
+ *																		   *
+ * This file is part of the MinDia package (program to make slide shows),  *
+ *																		   *
+ * Copyright (C) 2002-2003 by Michael Neuroth.							   *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 2 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _APPCONFIG_H
+#define _APPCONFIG_H
+
+#include <string>
+
+using namespace std;
+
+string GetImagePath();
+string GetMusicPath();
+string GetDataPath();
+
+#endif
