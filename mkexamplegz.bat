5a043d2e7680f1c81fe3ad50422df98d7f490e3f
min <min@5f0a215f-9dc6-43d0-aa08-c0b9dc37ed8b>
2004-01-18 23:53:51 +0000

    Scripts to generate example package-files.
    git-svn-id: file:///Users/min/Documents/svnrepros/svn_mindia/trunk@86 5f0a215f-9dc6-43d0-aa08-c0b9dc37ed8b

---
+++ mkexamplegz.bat
@@ -0,0 +1,2 @@
+tar -c -f example001.tar example.txt data/example_nz.dia music/songd_32b.mp3 images/nz_*.jpg
+gzip example001.tar
---
+++ mkexamplezip.bat
@@ -0,0 +1,5 @@
+zip example001.zip example.txt
+zip example001.zip data/example_nz.dia
+zip example001.zip music/songd_32b.mp3
+zip example001.zip images/nz_*.jpg
+
