3b239cf517d29db6bbece48088fb4eceb5ac99e6
min <min@5f0a215f-9dc6-43d0-aa08-c0b9dc37ed8b>
2004-02-22 11:20:57 +0000

    jpg lib excluded
    git-svn-id: file:///Users/min/Documents/svnrepros/svn_mindia/trunk@136 5f0a215f-9dc6-43d0-aa08-c0b9dc37ed8b

--- dowzipn.bat
+++ dowzipn.bat
@@ -1,7 +1,7 @@
-zip -u %1 src/* minsrv/* mindiapyc/* gendev/* scripts/* zaurus/* 
+zip -u %1 src/* minsrv/* mindiapyc/* gendev/* scripts/* zaurus/* -x moc_*
 zip -u %1 *.html *.ico *.po *.pro *.qm *.dsp *.dsw *.bat *.iss *.ini *.m4 *.specs
 zip -u %1 mindiaall.mak wmindia.mak lmindia.mak *.mak Makefile 
 zip -u %1 AUTHORS COPYING HISTORY INSTALL BUGS README readme.txt example.txt 
-zip -u %1 *.txt *.in configure installapp dotgz setqt2 setqt3 jpg/inc/* jpg/jpgdlib/* jpg/lib/* -x moc_*
-
+zip -u %1 *.txt *.in configure installapp dotgz setqt2 setqt3 -x moc_*
+rem -u %1 jpg/inc/* jpg/jpgdlib/* jpg/lib/* 
 rem zip -u examples.zip *.dia *.bmp *.jpg
