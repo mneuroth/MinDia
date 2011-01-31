9aaf809eb39141e210999167d683be780345ec4c
min <min@5f0a215f-9dc6-43d0-aa08-c0b9dc37ed8b>
2011-01-31 21:16:40 +0000

    improved handling of source code archive tar.gz
    git-svn-id: file:///Users/min/Documents/svnrepros/svn_mindia/trunk@242 5f0a215f-9dc6-43d0-aa08-c0b9dc37ed8b

--- make_src_tar.sh
+++ make_src_tar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir mindia-1.0.0
-./do_file_list.sh
-tar -cf - -T file.lst | tar -xpf - -C mindia-1.0.0
+#./do_file_list.sh
+tar -cf - -T src_files.lst | tar -xpf - -C mindia-1.0.0
 tar -z -cf mindia-1.0.0.tar.gz mindia-1.0.0/*
 rm -rf mindia-1.0.0/
 #list: tar -tvf mindia2-1.0.0.tar.gz
