8a9f1d954008e6848b284ac0139d6d929ba08191
min <min@5f0a215f-9dc6-43d0-aa08-c0b9dc37ed8b>
2013-04-01 12:23:03 +0000

    remove unused code
    git-svn-id: file:///Users/min/Documents/svnrepros/svn_mindia/trunk@331 5f0a215f-9dc6-43d0-aa08-c0b9dc37ed8b

--- src/dynamictextitem.cpp
+++ src/dynamictextitem.cpp
@@ -3,10 +3,6 @@
 #include <QGraphicsScene>
 #include <QGraphicsWidget>
 
-#include <iostream>
-
-using namespace std;
-
 DynamicTextItem::DynamicTextItem( DynamicItemChangedCallback * pCallback )
     : m_pCallback(pCallback)
 {
@@ -18,8 +14,6 @@ DynamicTextItem::~DynamicTextItem()
 
 void DynamicTextItem::mouseMoveEvent( QGraphicsSceneMouseEvent * event )
 {
-    //cout << "move " << /*event->pos().*/scenePos().x() << "/" << x() << " " << /*event->pos().*/scenePos().y() << "/" << y() << endl;
-    //cout << "scene " << this->scene()->width() << " " << this->scene()->height() << endl;
     QGraphicsSimpleTextItem::mouseMoveEvent(event);
     if( m_pCallback )
     {
