2bfb1a9caa87a61a7d991dd6f25f71170f16d1df
min <min@5f0a215f-9dc6-43d0-aa08-c0b9dc37ed8b>
2013-05-14 17:28:14 +0000

    improved help dialog
    git-svn-id: file:///Users/min/Documents/svnrepros/svn_mindia/trunk@433 5f0a215f-9dc6-43d0-aa08-c0b9dc37ed8b

--- src/helpdlgimpl.cpp
+++ src/helpdlgimpl.cpp
@@ -22,133 +22,133 @@
 #
 // *******************************************************************
 
-/* XPM */
-/* Drawn  by Mark Donohoe for the K Desktop Environment */
-/* See http://www.kde.org */
-static const char*back[]={
-"16 16 5 1",
-"# c #000000",
-"a c #ffffff",
-"c c #808080",
-"b c #c0c0c0",
-". c None",
-"................",
-".......#........",
-"......##........",
-".....#a#........",
-"....#aa########.",
-"...#aabaaaaaaa#.",
-"..#aabbbbbbbbb#.",
-"...#abbbbbbbbb#.",
-"...c#ab########.",
-"....c#a#ccccccc.",
-".....c##c.......",
-"......c#c.......",
-".......cc.......",
-"........c.......",
-"................",
-"......................"};
+///* XPM */
+///* Drawn  by Mark Donohoe for the K Desktop Environment */
+///* See http://www.kde.org */
+//static const char*back[]={
+//"16 16 5 1",
+//"# c #000000",
+//"a c #ffffff",
+//"c c #808080",
+//"b c #c0c0c0",
+//". c None",
+//"................",
+//".......#........",
+//"......##........",
+//".....#a#........",
+//"....#aa########.",
+//"...#aabaaaaaaa#.",
+//"..#aabbbbbbbbb#.",
+//"...#abbbbbbbbb#.",
+//"...c#ab########.",
+//"....c#a#ccccccc.",
+//".....c##c.......",
+//"......c#c.......",
+//".......cc.......",
+//"........c.......",
+//"................",
+//"......................"};
 
-/* XPM */
-/* Drawn  by Mark Donohoe for the K Desktop Environment */
-/* See http://www.kde.org */
-static const char*forward[]={
-"16 16 5 1",
-"# c #000000",
-"a c #ffffff",
-"c c #808080",
-"b c #c0c0c0",
-". c None",
-"................",
-"................",
-".........#......",
-".........##.....",
-".........#a#....",
-"..########aa#...",
-"..#aaaaaaabaa#..",
-"..#bbbbbbbbbaa#.",
-"..#bbbbbbbbba#..",
-"..########ba#c..",
-"..ccccccc#a#c...",
-"........c##c....",
-"........c#c.....",
-"........cc......",
-"........c.......",
-"................",
-"................"};
+///* XPM */
+///* Drawn  by Mark Donohoe for the K Desktop Environment */
+///* See http://www.kde.org */
+//static const char*forward[]={
+//"16 16 5 1",
+//"# c #000000",
+//"a c #ffffff",
+//"c c #808080",
+//"b c #c0c0c0",
+//". c None",
+//"................",
+//"................",
+//".........#......",
+//".........##.....",
+//".........#a#....",
+//"..########aa#...",
+//"..#aaaaaaabaa#..",
+//"..#bbbbbbbbbaa#.",
+//"..#bbbbbbbbba#..",
+//"..########ba#c..",
+//"..ccccccc#a#c...",
+//"........c##c....",
+//"........c#c.....",
+//"........cc......",
+//"........c.......",
+//"................",
+//"................"};
 
-/* XPM */
-/* Drawn  by Mark Donohoe for the K Desktop Environment */
-/* See http://www.kde.org */
-static const char*home[]={
-"16 16 4 1",
-"# c #000000",
-"a c #ffffff",
-"b c #c0c0c0",
-". c None",
-"........... ....",
-"   ....##.......",
-"..#...####......",
-"..#..#aabb#.....",
-"..#.#aaaabb#....",
-"..##aaaaaabb#...",
-"..#aaaaaaaabb#..",
-".#aaaaaaaaabbb#.",
-"###aaaaaaaabb###",
-"..#aaaaaaaabb#..",
-"..#aaa###aabb#..",
-"..#aaa#.#aabb#..",
-"..#aaa#.#aabb#..",
-"..#aaa#.#aabb#..",
-"..#aaa#.#aabb#..",
-"..#####.######..",
-"................"};
+///* XPM */
+///* Drawn  by Mark Donohoe for the K Desktop Environment */
+///* See http://www.kde.org */
+//static const char*home[]={
+//"16 16 4 1",
+//"# c #000000",
+//"a c #ffffff",
+//"b c #c0c0c0",
+//". c None",
+//"........... ....",
+//"   ....##.......",
+//"..#...####......",
+//"..#..#aabb#.....",
+//"..#.#aaaabb#....",
+//"..##aaaaaabb#...",
+//"..#aaaaaaaabb#..",
+//".#aaaaaaaaabbb#.",
+//"###aaaaaaaabb###",
+//"..#aaaaaaaabb#..",
+//"..#aaa###aabb#..",
+//"..#aaa#.#aabb#..",
+//"..#aaa#.#aabb#..",
+//"..#aaa#.#aabb#..",
+//"..#aaa#.#aabb#..",
+//"..#####.######..",
+//"................"};
 
-static const char*findsearch[]={
-"16 16 4 1",
-"# c #000000",
-"a c #ffffff",
-"b c #c0c0c0",
-". c None",
-"................",
-"................",
-".....######.....",
-"....##bbbb##....",
-"...##bbbbbb##...",
-"...##bbbbbb##...",
-"...##bbbbbb##...",
-"....##bbbb##....",
-".....######.....",
-"........###.....",
-".........###....",
-"..........###...",
-"..........###...",
-"...........###..",
-"...........###..",
-"................"};
+//static const char*findsearch[]={
+//"16 16 4 1",
+//"# c #000000",
+//"a c #ffffff",
+//"b c #c0c0c0",
+//". c None",
+//"................",
+//"................",
+//".....######.....",
+//"....##bbbb##....",
+//"...##bbbbbb##...",
+//"...##bbbbbb##...",
+//"...##bbbbbb##...",
+//"....##bbbb##....",
+//".....######.....",
+//"........###.....",
+//".........###....",
+//"..........###...",
+//"..........###...",
+//"...........###..",
+//"...........###..",
+//"................"};
 
-static const char*closeicon[]={
-"16 16 4 1",
-"# c #FF0000",
-"a c #ffffff",
-"b c #c0c0c0",
-". c None",
-"................",
-"................",
-"..##........##..",
-"...##......##...",
-"....##....##....",
-".....##..##.....",
-"......####......",
-".......##.......",
-".......##.......",
-"......####......",
-".....##..##.....",
-"....##....##....",
-"...##......##...",
-"..##........##..",
-"................",
-"................"};
+//static const char*closeicon[]={
+//"16 16 4 1",
+//"# c #FF0000",
+//"a c #ffffff",
+//"b c #c0c0c0",
+//". c None",
+//"................",
+//"................",
+//"..##........##..",
+//"...##......##...",
+//"....##....##....",
+//".....##..##.....",
+//"......####......",
+//".......##.......",
+//".......##.......",
+//"......####......",
+//".....##..##.....",
+//"....##....##....",
+//"...##......##...",
+//"..##........##..",
+//"................",
+//"................"};
 
 // *******************************************************************
 // *******************************************************************
@@ -159,16 +159,20 @@ HelpDlgImpl::HelpDlgImpl( QWidget* parent, Qt::WFlags fl )
 {
     setupUi(this);
 
-    QPixmap aBackIcon( back );
-	QPixmap aForwardIcon( forward );
-	QPixmap aHomeIcon( home );
-	QPixmap aFindIcon( findsearch );
-	QPixmap aCloseIcon( closeicon );
-    m_pBack->setIcon( aBackIcon );
-    m_pForward->setIcon( aForwardIcon );
-    m_pHome->setIcon( aHomeIcon );
-    m_pSearch->setIcon( aFindIcon );
-    m_pClose->setIcon( aCloseIcon );
+    //QPixmap aBackIcon( back );
+    //QPixmap aForwardIcon( forward );
+    //QPixmap aHomeIcon( home );
+    //QPixmap aFindIcon( findsearch );
+    //QPixmap aCloseIcon( closeicon );
+    //m_pBack->setIcon( aBackIcon );
+    //m_pForward->setIcon( aForwardIcon );
+    //m_pHome->setIcon( aHomeIcon );
+    //m_pSearch->setIcon( aFindIcon );
+    //m_pClose->setIcon( aCloseIcon );
+
+    m_pBack->setEnabled(false);
+    m_pForward->setEnabled(false);
+    m_pHome->setEnabled(false);
 
 	m_iParagraph = 0;
 	m_iIndex = 0;
