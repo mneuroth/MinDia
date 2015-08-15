47b143c19d47d260bd297f37a00ba38a73e12144
min <min@5f0a215f-9dc6-43d0-aa08-c0b9dc37ed8b>
2015-08-15 18:39:33 +0000

    Updated version number
    git-svn-id: file:///Users/min/Documents/svnrepros/svn_mindia/trunk@482 5f0a215f-9dc6-43d0-aa08-c0b9dc37ed8b

--- make_src_tar.sh
+++ make_src_tar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-mkdir mindia-0.99.1
+mkdir mindia-0.99.2
 #./do_file_list.sh
-tar -cf - -T new_src_files.lst | tar -xpf - -C mindia-0.99.1
-tar -z -cf mindia-0.99.1.tar.gz mindia-0.99.1/*
-rm -rf mindia-0.99.1/
+tar -cf - -T new_src_files.lst | tar -xpf - -C mindia-0.99.2
+tar -z -cf mindia-0.99.2.tar.gz mindia-0.99.2/*
+rm -rf mindia-0.99.2/
 #list: tar -tvf mindia2-0.99.1.tar.gz
