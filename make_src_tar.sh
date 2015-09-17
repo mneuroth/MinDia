f54c5aceddf38e5319259f306b6841df20ec1e17
min <min@5f0a215f-9dc6-43d0-aa08-c0b9dc37ed8b>
2015-09-17 20:19:11 +0000

    Remove ._* files from MacOS
    git-svn-id: file:///Users/min/Documents/svnrepros/svn_mindia/trunk@558 5f0a215f-9dc6-43d0-aa08-c0b9dc37ed8b

--- make_src_tar.sh
+++ make_src_tar.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir mindia-0.99.2
 #./do_file_list.sh
-tar -cf - -T new_src_files.lst | tar -xpf - -C mindia-0.99.2
-tar -z -cf mindia-0.99.2.tar.gz mindia-0.99.2/*
+tar --disable-copyfile -cf - -T new_src_files.lst | tar --disable-copyfile -xpf - -C mindia-0.99.2
+tar --disable-copyfile -z -cf mindia-0.99.2.tar.gz mindia-0.99.2/*
 rm -rf mindia-0.99.2/
 #list: tar -tvf mindia2-0.99.1.tar.gz
